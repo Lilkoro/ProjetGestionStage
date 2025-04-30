340b349e67ec1f4380d0b8881eb56142d22fd4ee
HowYouDoIt <87282972+Lilkoro@users.noreply.github.com>
2025-04-30 00:53:27 -0600

    First push

---
+++ code/ProjetGestion/ProjetGestion/route.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetGestion
+{
+    class route
+    {
+        public class Entreprise
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+        public class API
+        {
+            public List<Entreprise> Entreprises { get; private set;} = new List<Entreprise>();
+    }
+
+    }
+}
